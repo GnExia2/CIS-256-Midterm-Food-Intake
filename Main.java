f4b8be20ad626e1e32743cbb9fb187fd20fe914a
GnExia2 <ngai.garrick.j@gmail.com>
2023-10-24 18:17:44 +0800

    refactored and deleted unneeded code

--- Main.java
+++ Main.java
@@ -2,10 +2,5 @@ public class Main {
     public static void main(String[] args) {
         FoodIntakeTrackerApp app = new FoodIntakeTrackerApp();
         app.start();
-
-        FoodDatabase foodDatabase = new FoodDatabase();
-        foodDatabase.addFoodItem("Chicken Salad", 350); // Example food item with calories
-        foodDatabase.addFoodItem("Pasta", 450);
-
     }
 }
